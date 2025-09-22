2f0834cd90f3e86b99859af72c0c00bc693394ba
Abdunazar <abdunazaribragimov945@gmail.com>
2025-09-22 15:58:34 +0500

    Third commit

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,13 +3,10 @@ import { adminLogin, adminLogout, refreshToken } from "../controllers/auth.contr
 
 const router = Router();
 
-// login
 router.post("/login", adminLogin);
 
-// logout
 router.post("/logout", adminLogout);
 
-// refresh token
 router.get("/refresh", refreshToken);
 
 export default router;
